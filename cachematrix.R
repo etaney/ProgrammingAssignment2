fc4b25e20a5dc8f522c519ef6659e39ff4aae78f
etaney <contact@erictaney.com>
2016-07-09 22:14:37 -0700

    Filled in functions
    makeCacheMatrix now dumps the matrix into a variable and stores that in
    a list
    
    cacheSolve now extracts that list item and inverts it

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,14 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Dumps externally supplied matrix 'y' into a variable 'set' or extracts 'y' with 'get'
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  y <<- x      #store external variable x in y
+  
+  get <- function() y  #get function returns 'y'
+  list(get=get) #list with item 'get' uses function 'get' to return y
 
 }
 
@@ -12,4 +17,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  input <- x$get() #get the cached matrix from x
+  inverse <- solve(input) #invert the cached matrix and dump it in 'inverse'
+  inverse # return 'inverse'
+  
 }
